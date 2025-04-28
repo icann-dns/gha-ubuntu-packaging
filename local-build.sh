ea0944d1deb196177e923bb7bdaa726672a1cac3
jbond <github@johnbond.org>
2025-04-28 15:42:47 +0200

    fix permissions

--- local-build.sh
+++ local-build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo chown -R "$(whoami)" "${PWD}/"
 uscan --verbose --force-download --download-current-version
 sudo mk-build-deps --remove  --install --tool="apt-get -o Debug::pkgProblemResolver=yes --no-install-recommends --yes" debian/contro
 sudo rm -rf ./*deps*.deb
