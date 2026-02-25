21679cb76d9f7e3b1ba4b29787e374356a512427
jbond <github@johnbond.org>
2026-02-25 17:43:03 +0100

    Revert "usscan before cutting new release"
    This reverts commit 8f35d8fdde8d2685c3cea3fa0db4933d9b2bc505.

--- local-build.sh
+++ local-build.sh
@@ -3,11 +3,9 @@ set -x
 sudo chown -R "$(whoami)" "${PWD}/"
 version=$(awk 'NR==1{gsub(/[()]/,"",$2); print $2 }' debian/changelog)
 source /etc/lsb-release
-# Do this before we update the version
-sudo uscan --verbose --force-download --download-current-version
 dch --distribution "${DISTRIB_CODENAME}" --newversion "${version}+ubuntu${DISTRIB_RELEASE}" "Github build of ${DISTRIB_CODENAME} packages"
 sudo mk-build-deps --remove --install --tool="apt-get -o Debug::pkgProblemResolver=yes --no-install-recommends --yes" debian/control
 sudo rm -rf ./*deps*.{deb,buildinfo,changes}
-dpkg-buildpackage -us -uc
+dpkg-buildpackage -b -us -uc
 mkdir -p ${RESULT_DIR}
 mv ../*deb ${RESULT_DIR}
